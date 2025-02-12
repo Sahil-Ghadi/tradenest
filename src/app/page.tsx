cc7d0d8f54f5e36b7b7aca52f6661ef559a0ece7
Shridhar Mandrekar <geekyshridhar04@gmail.com>
2025-02-12 15:55:39 +0530

    added user mathods

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-"use client"
+"use client";
 
 import axios from "axios";
-import { useState,useEffect } from "react";
-import ProductCard from "@/components/ProductCard"
+import { useState, useEffect } from "react";
+import ProductCard from "@/components/ProductCard";
 
 export default function Home() {
-
   // interface itemSlot {
   //   _id:string,
   //   name:string,
@@ -13,22 +12,22 @@ export default function Home() {
   //   imageUrl:string,
   // }
 
-  const [items,setItems] = useState([])
+  const [items, setItems] = useState([]);
 
   const fetchItems = async () => {
-   try {
-     const res = await axios.get('/api/items')
-     setItems(res.data)
-     console.log("items fetched ",res.data);
-   } catch (error) {
-    console.log("error in fetching items ",error);
-   }
-  }
+    try {
+      const res = await axios.get("/api/items");
+      setItems(res.data);
+      console.log("items fetched ", res.data);
+    } catch (error) {
+      console.log("error in fetching items ", error);
+    }
+  };
 
   useEffect(() => {
-   fetchItems()
-  }, [items])
-  
+    fetchItems();
+  }, [items]);
+
   return (
     <main className="container mx-auto px-4 py-8">
       <h1 className="mb-8 text-3xl font-bold">Featured Products</h1>
@@ -38,5 +37,5 @@ export default function Home() {
         ))}
       </div>
     </main>
-  )
+  );
 }
